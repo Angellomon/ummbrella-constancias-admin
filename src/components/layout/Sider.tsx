f99aea26188bc3628d91671d37cfcf4c09ca36df
Angellomon <angelmtzdiaz@gmail.com>
2021-07-26 02:33:53 -0500

    feat(eventos): Listado de Eventos en Sidebar

--- src/components/layout/Sider.tsx
+++ src/components/layout/Sider.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from "react";
-import { ExportOutlined, UserOutlined } from "@ant-design/icons";
-
+import { ExportOutlined, PlusOutlined, UserOutlined } from "@ant-design/icons";
+import { Link, NavLink, useHistory } from "react-router-dom";
 import { Layout, Menu } from "antd";
 import { useMenu } from "../../hooks";
 import Logo from "../../assets/logo-comecarne.png";
 import styled from "styled-components";
 import { useAuth } from "../../hooks/auth";
+import { useEventos } from "../../hooks/eventos";
+
 const { Sider } = Layout;
 
 const StlyedSider = styled(Sider)`
@@ -23,13 +25,25 @@ const StyledLogo = styled.img`
 const SiderPrincipal: FC = () => {
   const { logout } = useAuth();
   const { isCollapsed, toggle } = useMenu();
+
+  const { eventos } = useEventos();
+  const history = useHistory();
+
   return (
     <StlyedSider collapsible collapsed={isCollapsed} onCollapse={toggle}>
       <div className="logo" />
-      <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+      <Menu theme="dark" mode="inline">
         <StyledLogo alt="logo" src={Logo} />
-        <Menu.Item key="1" icon={<UserOutlined />}>
-          Asistentes
+        {eventos?.map((e) => (
+          <Menu.Item
+            key={e.clave}
+            onClick={() => history.push(`/evento/${e.clave}`)}
+          >
+            {e.nombre}
+          </Menu.Item>
+        ))}
+        <Menu.Item key="add_evento" icon={<PlusOutlined />}>
+          Nuevo Evento
         </Menu.Item>
         <Menu.Item key="2" icon={<ExportOutlined />} onClick={() => logout()}>
           Cerrar Sesión
