e7d536c7ac71cd658dd2bd8122b8b52c91e4d600
Angellomon <angelmtzdiaz@gmail.com>
2022-07-14 16:37:07 -0500

    chore: update react-router + adaptaciones

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "../context/auth";
 import { SWRConfig } from "swr";
 
@@ -7,38 +7,35 @@ import { PageLogin } from "./Login";
 import { doGet } from "../util/fetchers";
 import ProtectedComponent from "../components/oauth/ProtectedComponent";
 import { Scope } from "../schemas/oauth";
-import { AsistentesPage } from "./asistentes";
-import { AsistentesProvider } from "../context/asistentes";
 import { default as DashboardLayout } from "../components/layout/Dashboard";
 import { PageEvento } from "./eventos";
 import EventoProvider from "../context/eventos/EventoContext";
 
 const Dashboard: FC = () => {
   return (
-    <Switch>
-      <Route path="/login" component={PageLogin} />
-      <AuthProvider>
-        <SWRConfig
-          value={{
-            fetcher: doGet,
-          }}
-        >
-          <DashboardLayout>
+    <AuthProvider>
+      <SWRConfig
+        value={{
+          fetcher: doGet,
+        }}
+      >
+        <DashboardLayout>
+          <Routes>
+            <Route path="/login" element={<PageLogin />} />
             <Route
               path="/evento/:claveEvento"
-              exact
-              render={({ match }) => (
+              element={
                 <ProtectedComponent scopes={[Scope.READ_ASISTENTES]}>
-                  <EventoProvider claveEvento={match.params.claveEvento}>
+                  <EventoProvider>
                     <PageEvento />
                   </EventoProvider>
                 </ProtectedComponent>
-              )}
+              }
             />
-          </DashboardLayout>
-        </SWRConfig>
-      </AuthProvider>
-    </Switch>
+          </Routes>
+        </DashboardLayout>
+      </SWRConfig>
+    </AuthProvider>
   );
 };
 
